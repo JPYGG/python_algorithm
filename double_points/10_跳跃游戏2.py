09e1486293c153119ad92b8052c108b477acd2e4
ganqinren <1296528123@qq.com>
2026-03-31 20:26:09 +0800

    跳跃游戏

--- double_points/10_跳跃游戏2.py
+++ double_points/10_跳跃游戏2.py
@@ -1,21 +1,39 @@
 def jump(nums: list[int]) -> int:
     n = len(nums)
-    if n <= 1:
+    if n == 1:
         return 0
-    max_reach = 0
+    if n == 2:
+        return 1
     step = 0
-    for i in range(n):
-        if i + nums[i] >= max_reach:
-            max_reach = max(max_reach, i + nums[i])
+    current_end = 0
+    farthest = 0
+    for i in range(n - 1):
+        farthest = max(farthest, i + nums[i])
+        if i == current_end:
             step += 1
-        if max_reach >= n - 1:
-            return step
+            current_end = farthest
     return step
-
+# [7,0,9,6,9,6,1,7,9,0,1,2,9,0,3]
+# [2,3,1]
+# [3,2,1]
+# [1,2]
+# [1,1,1,1,1]
 # [1,2,1,1,1]
-nums = [2,3,1,1,4]
+# [2,3,1,1,4]
+nums = [3,2,1]
 print(jump(nums))
 
+'''
+纸面操作记录
+测试用例保证都能达到 n-1 处
+这里其中就是为了找出一个最短路径 到达 n-1 处
+引入一个 step 步长变量代表当前走了多少步
+然后再在 step 步长值内引入第二个循环来查找步长内的数能达到的最远距离 
+第一个循环应该用while条件控制，当 max >= n-1 时跳出循环
+第二个循环结束后，拿到当前步长内的最优距离下标， 当 max 变化的时候执行 step+1 操作
 
+
+
+'''
 
 
